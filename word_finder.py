35d4164c4251e947ba9a700b6b1ba52c0786fa68
Michael Harty <michaelharty3@gmail.com>
2020-06-19 12:01:22 -0500

    refactor word_finder to make check_word its own function

--- word_finder.py
+++ word_finder.py
@@ -7,41 +7,52 @@ def letters_in_word(letters, word):
         if l in word:
             return True
     return False
+
+
+def check_word(word, allowed_letters):
+    """
+    Check if a word meets the critera for the spelling bee game:
+        * The word may only contain the allowed_letters.
+        * The first letter in allowed_letters is the key letter. The word
+          must contain it.
+        * The words must be four characters or more in length.
+    """
+    key_letter = allowed_letters[0] # first letter is "key." it must be contained
+    bad_letters = re.sub(f'[{allowed_letters}]', '', ascii_lowercase) # get all un-allowed letters
     
+    word = word.lower()
+    if len(word) < 4:
+        return False 
+    if key_letter not in word:
+        return False
+    if letters_in_word(bad_letters, word):           
+        return False
+    
+    return True
 
 def word_finder(allowed_letters):
-    """Return a list of words that meet the spelling bee criteria.
+    """
+    Return a list of words that meet the critera for the spelling bee game.
         * The words may only contain the allowed_letters.
         * The first letter in allowed_letters is the key letter. Returned words
           must contain it.
-        * Words must be for characters or more in length.
+        * Words must be four characters or more in length.
     """
-    
-    key_letter = allowed_letters[0] # first letter is "key." it must be contained
-    bad_letters = re.sub(f'[{allowed_letters}]', '', ascii_lowercase) # get all un-allowed letters
 
     with open('data/norvig_word_list.txt') as f: # http://norvig.com/ngrams/word.list
         word_list = [word.strip() for word in f.readlines()]
 
     good_words = list()
     for word in word_list:
-        word = word.lower()
-
-        if len(word) < 4:
-            continue 
-        if key_letter not in word:
-            continue
-        if letters_in_word(bad_letters, word):           
-            continue
-        good_words.append(word)
+        if check_word(word, allowed_letters):
+            good_words.append(word)
 
     good_words.sort(key=len)
-    
     return good_words
 
-def find_pangrams(good_words):
+def find_pangrams(good_words, n=7):
     """Return a list of pangrams (ie words containing all allowed letters)
-        from a list of words"""
-    
+        from a list of words. """
+
     # there are 7 letters in the game, any word with 7 unique letters is a pangram
-    return [w for w in good_words if len(set(w)) == 7]
+    return [w for w in good_words if len(set(w)) == n]
